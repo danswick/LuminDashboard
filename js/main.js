cec7864b6118bee4e411cdbd876250f9863f9451
danswick <dan.swick@gmail.com>
2015-02-20 18:25:21 -0600

    style userID page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
-$('#idForm').submit(function( event ) {
-	window.location.href("http://deltalumin.com/webpage/?userID=" + $(this).name);
+$('#idForm').submit(function( e ) {
+	e.preventDefault();
+	console.log( $(this).serialize() );
+	location.href = 'http://deltalumin.com/webpage/?' + $(this).serialize();
+	console.log(ID);
 });
