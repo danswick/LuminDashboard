98aa447e7e715541b2f26db8ce533bc30ee760b3
Dan Swick <dan.swick@gmail.com>
2015-02-21 16:38:54 -0600

    add dashboard to nav, add check for valid ID

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 $('#idForm').submit(function( e ) {
 	e.preventDefault();
-	console.log( $(this).serialize() );
-	location.href = 'http://deltalumin.com/webpage/?' + $(this).serialize();
-	console.log(ID);
+	// check for valid ID. During testing, only < 5. 
+	if( ( parseInt($('#idForm .textInput').val()) > 0 ) && ( parseInt($('#idForm .textInput').val() ) < 5 )) {
+		console.log( $(this).serialize() );
+		location.href = 'http://deltalumin.com/webpage/?' + $(this).serialize();
+		console.log(ID);
+	} else {
+		alert("Please enter a valid ID number");
+	}
 });
+
+function setup() {
+	var textInput;
+	textInput = $('#idForm .textInput');
+	textInput.focus();
+}
+
+$(window).on('load', setup());
